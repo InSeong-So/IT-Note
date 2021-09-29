8f7e84ecb8b7000a6804cc379d516b22d1daae7c
InSeong-So <goflvhxj2547@gmail.com>
2021-09-29 17:09:46 +0900

    🔧 Javascript 코드를 위한 ESLint, Babel, Jest 추가

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  testMatch: ['<rootDir>/**/*.(js|jsx|ts|tsx)'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
